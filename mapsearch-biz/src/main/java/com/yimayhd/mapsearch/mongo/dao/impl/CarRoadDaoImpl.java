9e4a3e1e7793651be210d136b081cf99c6433b1a
liwei <liwei@jiuxiulvxing.com>
2016-10-10 14:24:49 +0800

    修改

--- mapsearch-biz/src/main/java/com/yimayhd/mapsearch/mongo/dao/impl/CarRoadDaoImpl.java
+++ mapsearch-biz/src/main/java/com/yimayhd/mapsearch/mongo/dao/impl/CarRoadDaoImpl.java
@@ -2,15 +2,17 @@ package com.yimayhd.mapsearch.mongo.dao.impl;
 
 
 import com.yimayhd.mapsearch.client.domain.mongo.CarPoint;
+import com.yimayhd.mapsearch.client.domain.mongo.CarRoad;
 import com.yimayhd.mapsearch.mongo.base.impl.BaseDaoImpl;
 import com.yimayhd.mapsearch.mongo.dao.CarPointDao;
+import com.yimayhd.mapsearch.mongo.dao.CarRoadDao;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 
-public class CarRoadDaoImpl extends BaseDaoImpl<CarPoint> implements CarPointDao{
+public class CarRoadDaoImpl extends BaseDaoImpl<CarRoad> implements CarRoadDao{
 	private static final Logger logger = Logger.getLogger(CarRoadDaoImpl.class);
     @Autowired
     private MongoTemplate mongoTemplate;
