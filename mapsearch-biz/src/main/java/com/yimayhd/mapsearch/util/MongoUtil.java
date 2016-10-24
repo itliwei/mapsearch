964f91d2a306e1613b11d55764e716cdc14da896
liwei <liwei@jiuxiulvxing.com>
2016-10-24 11:51:39 +0800

    修改工具类

--- mapsearch-biz/src/main/java/com/yimayhd/mapsearch/util/MongoUtil.java
+++ mapsearch-biz/src/main/java/com/yimayhd/mapsearch/util/MongoUtil.java
@@ -1,14 +1,5 @@
 package com.yimayhd.mapsearch.util;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Random;
